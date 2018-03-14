bfed33b1eb9989c17b25943aae1ba8776fae4732
raveendranv4629 <raveendranv4629@CA-SU-F106-33.sduhsd.lan>
2018-03-14 14:58:35 -0700

    Unit12

--- APCSA/src/Unit11GradesArrayList/ClassTester.java
+++ APCSA/src/Unit11GradesArrayList/ClassTester.java
@@ -6,9 +6,9 @@ public class ClassTester
 	public static void main(String args[]) 
 	{
 		Class test = new Class("Comp Sci 1");
-		test.addStudent(new Student("Jimmy", "4 - 100 90 80 60"));
-		test.addStudent(new Student("Sandy", "4 - 100 100 80 70"));
-		test.addStudent(new Student("Fred", "4 - 50 50 70 68"));
+		//test.addStudent(new Student("Jimmy", "4 - 100 90 80 60"));
+		//test.addStudent(new Student("Sandy", "4 - 100 100 80 70"));
+		//test.addStudent(new Student("Fred", "4 - 50 50 70 68"));
 		out.println(test);
 
 		out.println(String.format(test.getStudentName(0) + "\'s average = %.2f", test.getStudentAverage(0)));
