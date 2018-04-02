5304767610f0c3a54c7334faebbf39dc56cac34d
raveendranv4629 <raveendranv4629@CA-SU-F106-33.sduhsd.lan>
2018-04-02 14:59:18 -0700

    Pong

--- APCSA/src/Unit15Pong/SpeedUpBall.java
+++ APCSA/src/Unit15Pong/SpeedUpBall.java
@@ -8,54 +8,43 @@ class SpeedUpBall extends Ball
 {
 
    //instance variables
+	private int xSpeed;
+	private int ySpeed;
 
    public SpeedUpBall()
    {
-
-
+	   super();
    }
 
    public SpeedUpBall(int x, int y)
    {
-
-
+	   super(x, y);
    }
 
 
    public SpeedUpBall(int x, int y, int xSpd, int ySpd)
    {
-
-
+	   super(x, y, xSpd, ySpd);
    }
 
    public SpeedUpBall(int x, int y, int wid, int ht, int xSpd, int ySpd)
    {
-
-
+	   super(x, y, wid, ht, xSpd, ySpd);
    }
 
 
    public SpeedUpBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd)
    {
-
-
-
-
+	   super(x, y, wid, ht, col, xSpd, ySpd);
    }
 
    public void setXSpeed( int xSpd )
    {
-
-
-
-
+	   
    }
 
    public void setYSpeed( int ySpd )
    {
-
-
-
-
+	   
    }
 }
