720c885b3594fcf7616f7b593328e112b18cf3eb
raveendranv4629 <raveendranv4629@CA-SU-F106-33.sduhsd.lan>
2018-04-23 14:58:30 -0700

    Starfighter

---
+++ APCSA/src/Starfighter/Ship.java
@@ -0,0 +1,60 @@
+package Starfighter;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class Ship extends MovingThing
+{
+	private int speed;
+	private Image image;
+
+	public Ship()
+	{
+		this(0,0,0);
+	}
+
+	public Ship(int x, int y)
+	{
+		this(x,y,0);
+	}
+
+	public Ship(int x, int y, int s)
+	{
+		super(x, y);
+		speed=s;
+		try
+		{
+			image = ImageIO.read(new File("\\Users\\raveendranv4629\\Desktop\\VidhurWorkspace\\ship.jpg"));
+		}
+		catch(Exception e)
+		{
+			//feel free to do something here
+			System.out.println("Ship doesn't work!");
+		}
+	}
+
+
+	public void setSpeed(int s)
+	{
+	   //add more code
+		speed = s;
+	}
+
+	public int getSpeed()
+	{
+	   return speed;
+	}
+
+	public void draw( Graphics window )
+	{
+   	window.drawImage(image,getX(),getY(),80,80,null);
+	}
+
+	public String toString()
+	{
+		return super.toString() + getSpeed();
+	}
+}
